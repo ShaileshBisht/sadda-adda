ccfe5f2ae7b6135bb5b33d9a2935c546fefc8ead
shaileh Bisht <shaileshbisht765@gmail.com>
2021-03-15 20:03:02 +0530

    add text above hero section

--- src/component/HeroSection.js
+++ src/component/HeroSection.js
@@ -5,8 +5,13 @@ import ved from "../assets/video/hero.mp4";
 function HeroSection() {
   return (
     <div className="heroSection">
-      <div className="heroSection_video">
+      <div className="heroSection__video">
         <video className="video" autoPlay muted loop src={ved}></video>
+      </div>
+      <div className="heroSection__discription ">
+        <div className="heroSection__discription-tagLine container">
+          <p>Food that taste like heaven in your mouth...</p>
+        </div>
       </div>
     </div>
   );
