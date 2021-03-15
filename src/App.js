24d5405ac58bc7e0fab774e2b8118463dccd325f
shaileh Bisht <shaileshbisht765@gmail.com>
2021-03-15 18:00:17 +0530

    create Hero section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
+import HeroSection from "./component/HeroSection";
 import NavBar from "./component/NavBar";
 
 function App() {
   return (
     <div className="app">
       <NavBar />
+      <HeroSection />
     </div>
   );
 }
---
+++ src/component/HeroSection.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./HeroSection.css";
+
+function HeroSection() {
+  return (
+    <div className="heroSection">
+      <h1>i am Hero section</h1>
+    </div>
+  );
+}
+
+export default HeroSection;
