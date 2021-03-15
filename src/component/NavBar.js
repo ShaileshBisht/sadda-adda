1731aef9647b3fc87d15f22ba78e8031804fdd2f
shaileh Bisht <shaileshbisht765@gmail.com>
2021-03-15 09:44:01 +0530

    create NavBar component

---
+++ src/component/NavBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./NavBar.css";
+
+function NavBar() {
+  return (
+    <div className="navBar">
+      <h1>navBar</h1>
+    </div>
+  );
+}
+
+export default NavBar;
