e37f96ea2a6af1f6b919f7f433fe8de679fb6b6f
shaileh Bisht <shaileshbisht765@gmail.com>
2021-03-15 09:58:52 +0530

    add navBar menu

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -4,7 +4,28 @@ import "./NavBar.css";
 function NavBar() {
   return (
     <div className="navBar">
-      <h1>navBar</h1>
+      <div className="navBar-container">
+        <div className="navBar__logo">
+          <h1>sadda-adda</h1>
+        </div>
+        <div className="navBar__menu">
+          <a href="#" className="navBar__link">
+            home
+          </a>
+          <a href="#" className="navBar__link">
+            menu
+          </a>
+          <a href="#" className="navBar__link">
+            chef
+          </a>
+          <a href="#" className="navBar__link">
+            about
+          </a>
+          <a href="#" className="navBar__link">
+            contact
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
