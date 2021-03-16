87f8308b9f097c860f3680ebe35251ebd1dda18e
shaileh Bisht <shaileshbisht765@gmail.com>
2021-03-16 22:13:30 +0530

    correct padding issues in menu

--- src/component/Menu.js
+++ src/component/Menu.js
@@ -4,18 +4,20 @@ import MenuCard from "./MenuCard";
 
 function Menu() {
   return (
-    <div className="menu container">
-      <div className="menu__title">
-        <h1>our menu</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, et.
-        </p>
-      </div>
-      <div className="menu__card">
-        <MenuCard />
-        <MenuCard />
-        <MenuCard />
-        <MenuCard />
+    <div className="menu ">
+      <div className="menu__box container">
+        <div className="menu__title">
+          <h1>our menu</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, et.
+          </p>
+        </div>
+        <div className="menu__card">
+          <MenuCard />
+          <MenuCard />
+          <MenuCard />
+          <MenuCard />
+        </div>
       </div>
     </div>
   );
