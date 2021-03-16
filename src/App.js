631586dfe8010dc76968544d79dab2064936b649
shaileh Bisht <shaileshbisht765@gmail.com>
2021-03-16 14:11:42 +0530

    add title to menu section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import HeroSection from "./component/HeroSection";
 import NavBar from "./component/NavBar";
+import Menu from "./component/Menu";
 
 function App() {
   return (
     <div className="app">
       <NavBar />
       <HeroSection />
+      <Menu />
     </div>
   );
 }
---
+++ src/component/Menu.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Menu.css";
+
+function Menu() {
+  return (
+    <div className="menu container">
+      <div className="menu__title">
+        <h1>our menu</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, et.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Menu;
