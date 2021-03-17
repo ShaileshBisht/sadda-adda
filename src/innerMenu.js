188b301e7eaefdb848092411f54a259b7430d84f
shaileh Bisht <shaileshbisht765@gmail.com>
2021-03-17 21:40:22 +0530

    add json to innerMenu

---
+++ src/innerMenu.js
@@ -0,0 +1,18 @@
+const innerMenu = [
+  {
+    id: 1,
+    type:"burger";
+    name: "king burger",
+    price: 100;
+    discription:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo, eum?"
+  },{
+    id: 2,
+    type:"burger";
+    name: "queen burger",
+    price: 150;
+    discription:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo, eum?"
+  },
+  
+];
+
+export default innerMenu;
