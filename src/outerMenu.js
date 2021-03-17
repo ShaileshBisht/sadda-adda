70cf074471d96f51b4e31842396614a239587e7d
shaileh Bisht <shaileshbisht765@gmail.com>
2021-03-17 11:56:24 +0530

    change grid layout

--- src/outerMenu.js
+++ src/outerMenu.js
@@ -1,15 +1,52 @@
 import img1 from "./assets/menu/burger.jpg";
+import img2 from "./assets/menu/icecream.jpg";
 import img3 from "./assets/menu/juice.jpg";
 import img4 from "./assets/menu/momos.jpg";
 import img5 from "./assets/menu/noodles.jpg";
 import img6 from "./assets/menu/rice.jpg";
-import img2 from "./assets/menu/icecream.jpg";
+import img7 from "./assets/menu/soup.jpg";
+import img8 from "./assets/menu/rolls.jpg";
 
 const outerMenu = [
   {
     id: 1,
     name: "burger",
     imgSrc: img1,
+  },
+  {
+    id: 2,
+    name: "icecream",
+    imgSrc: img2,
+  },
+  {
+    id: 3,
+    name: "juice",
+    imgSrc: img3,
+  },
+  {
+    id: 4,
+    name: "momos",
+    imgSrc: img4,
+  },
+  {
+    id: 5,
+    name: "noodles",
+    imgSrc: img5,
+  },
+  {
+    id: 6,
+    name: "rice",
+    imgSrc: img6,
+  },
+  {
+    id: 7,
+    name: "soups",
+    imgSrc: img7,
+  },
+  {
+    id: 8,
+    name: "rolls",
+    imgSrc: img8,
   },
 ];
 
