73dd41d50080c0debebca1524b3fcbc8c276b4e8
shaileh Bisht <shaileshbisht765@gmail.com>
2021-03-17 21:33:22 +0530

    use History to push page to SubMenu

--- src/component/MenuCard.js
+++ src/component/MenuCard.js
@@ -2,12 +2,15 @@ import React from "react";
 import "./MenuCard.css";
 import { useDispatch } from "react-redux";
 import { menuTitle } from "../features/counter/menuSlice";
+import { useHistory } from "react-router-dom";
 
 function MenuCard({ name, imgSrc }) {
   const disptach = useDispatch();
+  const history = useHistory();
 
   const subMenu = () => {
     disptach(menuTitle(name));
+    history.push("/submenu");
   };
 
   return (
