1b1eaca53614594340eed2a22fd84ecfe542ebe4
Shailesh Bisht <shailesh.bisht@ibm.com>
2021-03-19 17:06:48 +0530

    add style to ChefCard

--- src/component/ChefCard.js
+++ src/component/ChefCard.js
@@ -1,10 +1,22 @@
 import React from "react";
 import "./ChefCard.css";
+import chef1 from "../assets/chef/chef1.jpg";
 
 function ChefCard() {
   return (
     <div className="chefCard">
-      <h1>i am chefCard</h1>
+      <div className="chefCard__box">
+        <div className="chefCardBox-inner">
+          <img src={chef1} alt="" />
+        </div>
+        <div className="chefCardBox-outer">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius
+            magnam in magni reiciendis. Tempore, quo. Quisquam eum accusantium
+            architecto neque?
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
