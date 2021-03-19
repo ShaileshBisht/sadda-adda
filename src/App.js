22b4d9a5c4b27f5a53504679c450b453af88f6da
Shailesh Bisht <shailesh.bisht@ibm.com>
2021-03-19 16:18:57 +0530

    create chef component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Menu from "./component/Menu";
 import About from "./component/About";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import SubMenu from "./component/SubMenu";
+import Chef from "./component/Chef";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
             <HeroSection />
             <About />
             <Menu />
+            <Chef />
           </Route>
         </Switch>
       </Router>
---
+++ src/component/Chef.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Chef.css";
+
+function Chef() {
+  return (
+    <div className="chef">
+      <h1>i am chef component</h1>
+    </div>
+  );
+}
+
+export default Chef;
