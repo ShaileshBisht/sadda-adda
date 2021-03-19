4452bf2d7a8090c27c098f20d7a952d4f480e446
Shailesh Bisht <shailesh.bisht@ibm.com>
2021-03-19 14:18:52 +0530

    add background image to about section

--- src/component/About.js
+++ src/component/About.js
@@ -8,7 +8,16 @@ function About() {
         <div className="about__title">
           <h1>about us</h1>
         </div>
-        <div className="about__content"></div>
+        <div className="about__content">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            dignissimos omnis eaque, nesciunt porro sapiente eum molestias sequi
+            maxime, dicta reiciendis expedita adipisci. Quam hic distinctio
+            aliquid molestias fugit facere, exercitationem quos quidem eveniet
+            impedit dolorem magni laboriosam quaerat earum sint autem iusto
+            aperiam repellendus architecto natus. Debitis, unde error.
+          </p>
+        </div>
       </div>
     </div>
   );
