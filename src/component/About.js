d5da9ad77dda2317d492848423bff1e183f831f9
Shailesh Bisht <shailesh.bisht@ibm.com>
2021-03-19 13:56:07 +0530

    add style to about component title

--- src/component/About.js
+++ src/component/About.js
@@ -4,10 +4,11 @@ import "./About.css";
 function About() {
   return (
     <div className="about">
-      <div className="about__box">
+      <div className="about__box container">
         <div className="about__title">
-          <p>about us</p>
+          <h1>about us</h1>
         </div>
+        <div className="about__content"></div>
       </div>
     </div>
   );
