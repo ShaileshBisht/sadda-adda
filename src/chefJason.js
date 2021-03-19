55cb59ae35590ea4a6e82231745fc530d28df034
Shailesh Bisht <shailesh.bisht@ibm.com>
2021-03-19 17:44:19 +0530

    create chef details card

---
+++ src/chefJason.js
@@ -0,0 +1,26 @@
+import img1 from "./assets/chef/chef1.jpg";
+import img2 from "./assets/chef/chef2.jpg";
+import img3 from "./assets/chef/chef3.jpg";
+
+const chefDetails = [
+  {
+    id: 1,
+    name: "Saakar",
+    disp: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, et.",
+    imgSrc: img1,
+  },
+  {
+    id: 2,
+    name: "Shailesh",
+    disp: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, et.",
+    imgSrc: img2,
+  },
+  {
+    id: 1,
+    name: "vipul",
+    disp: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, et.",
+    imgSrc: img3,
+  },
+];
+
+export default chefDetails;
