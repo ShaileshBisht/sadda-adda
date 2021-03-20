87395b17359b00d5715a230c3b6030582b866ea8
Shailesh Bisht <shailesh.bisht@ibm.com>
2021-03-20 16:04:45 +0530

    add style to footer

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -4,39 +4,54 @@ import IconButton from "@material-ui/core/IconButton";
 import InstagramIcon from "@material-ui/icons/Instagram";
 import FacebookIcon from "@material-ui/icons/Facebook";
 import ExploreIcon from "@material-ui/icons/Explore";
+import WhatsAppIcon from "@material-ui/icons/WhatsApp";
 
 function Footer() {
   return (
     <div className="footer">
       <div className="footer__box container">
-        <div className="footer__left">
-          <span>thanks for visiting</span>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit,
-            tenetur?
-          </p>
-          <p>(+91)-1234567890</p>
-        </div>
-        <div className="footer__right">
-          <div className="footer__right-top">
-            <p>get to know us better</p>
+        <div className="footer__box-top">
+          <div className="footer__left">
+            <span>thanks for visiting!!</span>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit,
+              tenetur?
+            </p>
+            <p>(+91)-1234567890</p>
           </div>
-          <div className="footer__right-icon">
-            <IconButton aria-label="delete">
-              <a href="#">
-                <InstagramIcon />
-              </a>
-            </IconButton>
-            <IconButton aria-label="delete">
-              <a href="#">
-                <FacebookIcon />
-              </a>
-            </IconButton>
-            <IconButton aria-label="delete">
-              <a href="#">
-                <ExploreIcon />
-              </a>
-            </IconButton>
+          <div className="footer__right">
+            <div className="footer__right-title">
+              <p>get to know us better</p>
+            </div>
+            <div className="footer__right-icon">
+              <IconButton aria-label="delete">
+                <a href="#">
+                  <InstagramIcon fontSize="large" />
+                </a>
+              </IconButton>
+              <IconButton aria-label="delete">
+                <a href="#">
+                  <FacebookIcon fontSize="large" />
+                </a>
+              </IconButton>
+              <IconButton aria-label="delete">
+                <a href="#">
+                  <ExploreIcon fontSize="large" />
+                </a>
+              </IconButton>
+              <IconButton aria-label="delete">
+                <a href="#">
+                  <WhatsAppIcon fontSize="large" />
+                </a>
+              </IconButton>
+            </div>
+          </div>
+        </div>
+        <div className="footer__box-bottom">
+          <div className="footer__copyRight">
+            <p>
+              Copyright <span>&#169;</span> 2021 Sadda-Adda || Shailesh Bisht{" "}
+            </p>
           </div>
         </div>
       </div>
