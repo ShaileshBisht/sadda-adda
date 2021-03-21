c152a5386b0aeb5dcd09f6089763407d90f70ac4
shaileh Bisht <shaileshbisht765@gmail.com>
2021-03-21 20:22:02 +0530

    add offer caroursel

--- src/component/Offers.js
+++ src/component/Offers.js
@@ -1,8 +1,44 @@
 import React from "react";
 import "./Offers.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
+import offer1 from "../assets/offers/offer1.jpg";
+import offer2 from "../assets/offers/offer2.jpg";
+import offer3 from "../assets/offers/offer3.jpg";
+import offer4 from "../assets/offers/offer4.jpg";
 
 function Offers() {
-  return <div className="offers"></div>;
+  return (
+    <div className="offers container">
+      <Carousel
+        dynamicHeight={true}
+        infiniteLoop={true}
+        autoPlay={true}
+        showArrows={true}
+        transitionTime="1"
+        verticalSwipe="vertical"
+        emulateTouch={true}
+        swipeable={true}
+      >
+        <div>
+          <img src={offer1} alt="" />
+          <p className="legend">offers 1</p>
+        </div>
+        <div>
+          <img src={offer2} alt="" />
+          <p className="legend">offers 2</p>
+        </div>
+        <div>
+          <img src={offer3} alt="" />
+          <p className="legend">offers 3</p>
+        </div>
+        <div>
+          <img src={offer4} alt="" />
+          <p className="legend">offers 4</p>
+        </div>
+      </Carousel>
+    </div>
+  );
 }
 
 export default Offers;
