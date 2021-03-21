2fd85c275e46e7c32dc01211821c028173c71789
shaileh Bisht <shaileshbisht765@gmail.com>
2021-03-21 19:15:11 +0530

    offers component is created

--- src/App.js
+++ src/App.js
@@ -8,12 +8,16 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import SubMenu from "./component/SubMenu";
 import Chef from "./component/Chef";
 import Footer from "./component/Footer";
+import Offers from "./component/Offers";
 
 function App() {
   return (
     <div className="app">
       <Router>
         <Switch>
+          <Route exact path="/offers">
+            <Offers />
+          </Route>
           <Route exact path="/submenu">
             <SubMenu />
             <Footer />
---
+++ src/component/Offers.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Offers.css";
+
+function Offers() {
+  return (
+    <div className="offers">
+      <h1>i am offers</h1>
+    </div>
+  );
+}
+
+export default Offers;
